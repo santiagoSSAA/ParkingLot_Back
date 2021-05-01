6c55e77a1d7fb21ab51f319c348a1e9708e673dc
Santiago Sanchez <s.sanchez123@utp.edu.co>
2021-05-01 09:55:36 -0500

    fix: model name

--- api/models/password_recovery.py
+++ api/models/password_recovery.py
@@ -3,7 +3,7 @@
 from django.db import models
 from django_extensions.db.models import TimeStampedModel
 
-class PasswordRecovert(TimeStampedModel):
+class PasswordRecovery(TimeStampedModel):
     """
     Extends class TimeStampedModel.
     This class add fields created and modified
