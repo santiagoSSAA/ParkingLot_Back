71d03c2f28595abcea72538ae75d42decf3cc24e
Santiago Sanchez <s.sanchez123@utp.edu.co>
2021-05-24 16:45:26 -0500

    f

--- api/models/reservation.py
+++ api/models/reservation.py
@@ -1,7 +1,6 @@
 """ Contains the reservation model """
 
 from django.db import models
-from django.utils import datetime
 from django_extensions.db.models import TimeStampedModel
 
 from datetime import datetime
