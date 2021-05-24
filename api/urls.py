ea4633c5eb480f98ba5c38496fa4d6a54d345a3c
Santiago Sanchez <s.sanchez123@utp.edu.co>
2021-05-24 15:03:09 -0500

    urls

--- api/urls.py
+++ api/urls.py
@@ -19,9 +19,23 @@ from django.urls import path
 from .views.auth import AuthApi
 from .views.auth import RefreshTokenApi
 from .views.user.general import UserApi
+from .views.user.general import SpecificUserApi
+from .views.reservation import ReservationApi
+from .views.reservation import SpecificReservationApi
+from .views.parking_slot import ParkingSlotApi
+from .views.parking_slot import SpecificParkingSlotApi
+from .views.password_recovery import PasswordRecoveryApi
+from .views.password_recovery import SpecificPasswordRecoveryApi
 
 urlpatterns = [
     path("auth", AuthApi.as_view()),
     path('refresh_token/<str:refresh>', RefreshTokenApi.as_view()),
     path('user', UserApi.as_view()),
+    path('user/<int:id>', SpecificUserApi.as_view()),
+    path('reservation', ReservationApi.as_view()),
+    path('reservation/<int:id>', SpecificReservationApi.as_view()),
+    path('parking_slot', ParkingSlotApi.as_view()),
+    path('parking_slot/<int:id>', SpecificReservationApi.as_view()),
+    path('password_recovery', PasswordRecoveryApi.as_view()),
+    path('password_recovery/<str:token>', SpecificPasswordRecoveryApi.as_view()),
 ]
