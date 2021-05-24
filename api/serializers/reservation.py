d7ab002b84ced7423cead024340f3fd49e776301
Santiago Sanchez <s.sanchez123@utp.edu.co>
2021-05-24 16:30:29 -0500

    f

--- api/serializers/reservation.py
+++ api/serializers/reservation.py
@@ -8,8 +8,9 @@ from ..serializers.user.basic_info import BasicUserDisplaySerializer
 class ReservationSerializer(serializers.ModelSerializer):
     """ Defines Reservation serializer behaviour. """
     user = BasicUserDisplaySerializer()
+    status = serializers.CharField(source="get_status")
 
     class Meta: # pylint: disable=too-few-public-methods        
         model = Reservation
         fields = ["id", "user", "initial_hour", "final_hour", "vehicle_plate",
-        "vehicle_type", "slot", "document_number", "email", "is_cancelled"]
+        "vehicle_type", "slot", "document_number", "email", "status", "is_cancelled"]
