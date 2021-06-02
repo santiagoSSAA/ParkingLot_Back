0a076a50ee8b6faa932762306a8375e6e274afa4
Santiago Sanchez <s.sanchez123@utp.edu.co>
2021-06-02 11:42:33 -0500

    f

--- api/serializers/reservation.py
+++ api/serializers/reservation.py
@@ -8,21 +8,29 @@ from ..serializers.user.basic_info import BasicUserDisplaySerializer
 class ReservationClientSerializer(serializers.ModelSerializer):
     """ Defines Reservation serializer behaviour. """
     status = serializers.CharField(source="get_status")
+    number_plate = serializers.SerializerMethodField("get_plate")
     slot = serializers.SerializerMethodField("get_slot")
     class Meta: # pylint: disable=too-few-public-methods        
         model = Reservation
-        fields = ["id", "initial_hour", "final_hour", "vehicle_plate",
+        fields = ["id", "initial_hour", "final_hour", "number_plate",
         "vehicle_type", "slot", "status"]
 
     def get_slot(self, obj):
         return obj.slot.place_code
 
+    def get_plate(self, obj):
+        return obj.vehicle_plate
+
 class ReservationSerializer(serializers.ModelSerializer):
     """ Defines Reservation serializer behaviour. """
     user = BasicUserDisplaySerializer()
+    number_plate = serializers.SerializerMethodField("get_plate")
     status = serializers.CharField(source="get_status")
 
     class Meta: # pylint: disable=too-few-public-methods        
         model = Reservation
         fields = ["id", "user", "initial_hour", "final_hour", "vehicle_plate",
         "vehicle_type", "slot", "document_number", "email", "status", "is_cancelled"]
+
+    def get_plate(self, obj):
+        return obj.vehicle_plate
